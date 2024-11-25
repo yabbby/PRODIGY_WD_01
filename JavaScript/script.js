097ba24c93de44c576be7dad7abd06f5e2120009
yabbby <ye.aba.net@gmail.com>
2024-11-25 14:49:27 +0300

    inital commit

---
+++ JavaScript/script.js
@@ -0,0 +1,16 @@
+function showDescription(item) {
+    // Hide all descriptions
+    const descriptions = document.querySelectorAll('.description p');
+    descriptions.forEach(desc => desc.classList.add('hidden'));
+
+    // Show selected description
+    const selectedDescription = document.getElementById(item);
+    selectedDescription.classList.remove('hidden');
+}
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    window.scrollTo({
+        top: section.offsetTop - 60, // Adjust for the fixed navbar
+        behavior: 'smooth'
+    });
+}
